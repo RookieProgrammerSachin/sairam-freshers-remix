4238daf11d8352aabe94e6961d0397595f1f2a9d
Sachin / <sreesachin11226@gmail.com>
2024-06-08 07:28:55 +0530

    feat: tailwindcss

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,18 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Sairam Freshers Login" },
+    {
+      name: "description",
+      content: "Sairam Freshers — Get yourself accustomed",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1 className='text-3xl font-bold'>Hello world!</h1>
     </div>
   );
 }
